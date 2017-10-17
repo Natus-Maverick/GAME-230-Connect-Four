b90c4ae2534a47db88e44c67335e03025ee30f0c
Eric Guo <329593601@qq.com>
2017-10-17 15:16:18 -0700

    Fixed Wrap Around Option

--- Connect4/main.cpp
+++ Connect4/main.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 void board(); //print table
 int drop(int x, char symbol);	//drop to colunm x
-bool check(int x, int y, char s);	//check winner
+bool check(int x, int y, char s, char wrap);	//check winner
 char table[20][20];	//save board
 int x = 0;	//colunm
 int y = 0;	//line
@@ -22,6 +22,7 @@ char again; //new game check
 int row_size = 6;	//set size of the board
 int column_size = 7;
 int win_size = 4;
+char wrap;
 
 void board()		//print the board
 {
@@ -77,10 +78,20 @@ int main()		//main function
 	cin >> win_size;
 	while (cin.fail() || win_size < 3 || win_size >20 || win_size > row_size || win_size > column_size)		//Also avoid the win_size being larger than row or column size, even if it is not mentioned in Hard Mode "Objective I".
 	{
-		cout << "Please enter a valid number (4-20):";
+		cout << "Please enter a valid number (3-20):";
 		cin.clear();
 		cin.ignore(10000, '\n');
 		cin >> win_size;
+	}
+	char w;
+	cout << "Enable wrap around mode? (Y/N):\n";					//Choose enable wrap around mode
+	cin >> w;
+	while (w != 'y' && w != 'Y' && w != 'n' && w != 'N')
+	{
+		cout << "Please enter Y/y or N/n!\nEnable wrap around mode? (y/n):\n";
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cin >> w;
 	}
 	for (int i = 0; i < row_size; i++)		//empty the board
 	{
@@ -89,7 +100,7 @@ int main()		//main function
 			table[i][j] = 'I';
 		}
 	}
-	while (check(x, y, 'O') != true)		//Player 1 goes with "X", check win condition for Player 2 before player 1's turn.
+	while (check(x, y, 'O', w) != true)		//Player 1 goes with "X", check win condition for Player 2 before player 1's turn.
 	{
 		board();
 		int top = 0;
@@ -144,7 +155,7 @@ int main()		//main function
 			}
 		}
 		y = drop(x, 'X');		//If the drop if valid, record the row it was dropped
-		if (check(x, y, 'X') != true)		//Check win condition for Player 1 before Player 2's turn.
+		if (check(x, y, 'X', w) != true)		//Check win condition for Player 1 before Player 2's turn.
 		{
 			board();
 			if (table[0][0] != 'I' && table[0][1] != 'I' && table[0][2] != 'I' && table[0][3] != 'I' && table[0][4] != 'I' && table[0][5] != 'I' &&table[0][6] != 'I')
@@ -255,7 +266,7 @@ int drop(int x, char symbol)		//Drop symbol "X" or "O" in column x, and return t
 	return k;
 }
 
-bool check(int x, int y, char s)		//Check if "X" or "O" (declared by char s) win the game.
+bool check(int x, int y, char s, char wrap)		//Check if "X" or "O" (declared by char s) win the game.
 {
 	int horizonal = 0;
 	int vertical = 0;
@@ -264,18 +275,30 @@ bool check(int x, int y, char s)		//Check if "X" or "O" (declared by char s) win
 	int line;
 	int colunm;
 	char table_check[39][39];			//Maximun size is 39
-	for (int ri = 0; ri < row_size; ri++)	//Copy the board to check board.
+	if (wrap == 'Y' || wrap == 'y')
 	{
-		for (int ci = 0; ci < column_size; ci++)		//At the original positions.
+		for (int ri = 0; ri < row_size; ri++)	//Copy the board to check board.
 		{
-			table_check[ri][ci] = table[ri][ci];
-		}
-		for (int ci = column_size; ci < column_size + win_size -1; ci++)	//Connect the front columns with the last column to make a warp around check.
-		{
-			table_check[ri][ci] = table[ri][ci - column_size];
+			for (int ci = 0; ci < column_size; ci++)		//At the original positions.
+			{
+				table_check[ri][ci] = table[ri][ci];
+			}
+			for (int ci = column_size; ci < column_size + win_size - 1; ci++)	//Connect the front columns with the last column to make a warp around check.
+			{
+				table_check[ri][ci] = table[ri][ci - column_size];
+			}
 		}
 	}
-	
+	else
+	{
+		for (int ri = 0; ri < row_size; ri++)	//Copy the board to check board.
+		{
+			for (int ci = 0; ci < column_size; ci++)		//At the original positions.
+			{
+				table_check[ri][ci] = table[ri][ci];
+			}
+		}
+	}
 	for (colunm = x - 1; table_check[y - 1][colunm] == s && colunm <= 6; colunm--, horizonal++);
 	for (colunm = x; table_check[y - 1][colunm] == s && colunm >= 0; colunm++, horizonal++);
 	if (horizonal >= win_size)
